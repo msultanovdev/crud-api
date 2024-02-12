b8894e973fd630fb594cdeb6d621bdcf9b589af9
msultanovdev <mukhammadamin.sultanov@gmail.com>
2024-02-12 09:09:40 +0500

    fix: fix .env configs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 const http = require("http");
 import app from "./app";
-
+import * as dotenv from "dotenv";
+dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
